e39c52ca2dad98a37353a7ed863d0f4f41560e08
Jason Kim <iamjsonkim@gmail.com>
2020-07-24 01:17:52 -0700

    Footer links

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,34 @@
+import Link from "next/link";
+
+function functionFooterlink({ href, text, key }) {
+  return (
+    <a className="flex-initial pr-4 hover:underline" href={href} key={key}>
+      {text}
+    </a>
+  );
+}
+
 export default function Footer() {
+  const links = [
+    { href: "http://jasonkim.ca/", text: "Personal Site" },
+    { href: "https://github.com/serv", text: "Github" },
+    {
+      href: "https://www.linkedin.com/in/jason-kim-a2b97b6/",
+      text: "Linkedin"
+    },
+    { href: "https://twitter.com/jasoki", text: "Twitter" },
+    { href: "https://google.com", text: "RSS" }
+  ];
+
+  const linksComponents = [];
+  for (let i = 0; i < links.length; i++) {
+    let link = links[i];
+    linksComponents.push(functionFooterlink({ ...link, key: i + 1 }));
+  }
+
   return (
     <footer className="pb-10">
-      <div className="flex">
-        <div className="flex-initial pr-4">Personal Site</div>
-        <div className="flex-initial pr-4">Github</div>
-        <div className="flex-initial pr-4">Twitter</div>
-        <div className="flex-initial pr-4">LinkedIn</div>
-      </div>
+      <div className="flex">{linksComponents}</div>
     </footer>
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="py-8">
-      <h1>Jason Kim's Blog</h1>
+      <Link as={`/`} href="/">
+        <a className="hover:underline">
+          <h1>Jason Kim's Blog</h1>
+        </a>
+      </Link>
     </header>
   );
 }
