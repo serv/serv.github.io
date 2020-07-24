1a696b5bb0eab374f72a91612914822ce9593e77
Jason Kim <iamjsonkim@gmail.com>
2020-07-24 00:03:25 -0700

    Footer component

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="pb-10">
+      <div className="flex">
+        <div className="flex-initial pr-4">Personal Site</div>
+        <div className="flex-initial pr-4">Github</div>
+        <div className="flex-initial pr-4">Twitter</div>
+        <div className="flex-initial pr-4">LinkedIn</div>
+      </div>
+    </footer>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Posts from "../components/Posts";
 import Post from "../lib/post.js";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Home({ posts }) {
   return (
@@ -17,14 +18,7 @@ export default function Home({ posts }) {
         <Posts posts={posts} />
       </main>
 
-      <footer className="pb-10">
-        <div className="flex">
-          <div className="flex-initial pr-4">Personal Site</div>
-          <div className="flex-initial pr-4">Github</div>
-          <div className="flex-initial pr-4">Twitter</div>
-          <div className="flex-initial pr-4">LinkedIn</div>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
