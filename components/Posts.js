cb5ad768e4c6a157ec066c9278e3f7684ab5a98c
Jason Kim <iamjsonkim@gmail.com>
2022-04-08 15:13:52 -0700

    update date and title column widths

--- components/Posts.js
+++ components/Posts.js
@@ -1,18 +1,18 @@
-import React from "react";
-import moment from "moment";
-import Link from "next/link";
+import React from 'react';
+import moment from 'moment';
+import Link from 'next/link';
 
 export default function Posts({ posts }) {
   const result = [];
 
   for (let i = 0; i < posts.length; i++) {
     const post = posts[i];
-    const createdAt = moment(post.createdAt, "YYYY-MM-DD").format("YYYY-MM-DD");
+    const createdAt = moment(post.createdAt, 'YYYY-MM-DD').format('YYYY-MM-DD');
 
     result.push(
-      <div key={i} className="flex pb-4">
-        <div className="whitespace-no-wrap text-gray-600 pr-4">{createdAt}</div>
-        <div className="">
+      <div key={i} className="grid grid-cols-4 gap-4 pb-6">
+        <div className="whitespace-no-wrap text-gray-600">{createdAt}</div>
+        <div className="col-span-3">
           <Link as={`/post/${post.slug}`} href="/post/[slug]">
             <a className="hover:underline">{post.title}</a>
           </Link>
