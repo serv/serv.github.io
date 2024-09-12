774d26ed91b1fddb0c30aa1332bfe9a5d8830f51
Jason Kim <iamjsonkim@gmail.com>
2024-09-12 10:39:46 -0700

    Updates

---
+++ _next/static/4WY-UMc8Tf-x85odSKewV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/post/[slug]":[a,b]}}("static\u002Fchunks\u002F75fc9c18.cbe862d05f67479ee6e5.js","static\u002Fchunks\u002F2bf63edcc7ab1d40c193ebf1f9b6f43eba68441f.64565d5a878629f53174.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
